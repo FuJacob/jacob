9a4eb46e1cb70c997fdb611649baad68cd674578
Jacob Fu <jjacobfu@gmail.com>
2025-04-22 14:51:39 -0400

    editingmobile

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,17 +7,16 @@ import { motion } from "motion/react";
 export default function Home() {
   return (
     <motion.div
-      className="flex justify-center items-center h-screen "
+      className="flex sm:justify-center items-center h-screen "
       initial={{ scale: 0.95 }}
       whileHover={{ scale: 1 }}
       transition={{
         type: "spring",
         stiffness: 200,
-        mass: 0.3
-
+        mass: 0.3,
       }}
     >
-      <div className="h-screen sm:h-4/5 w-full overflow-y-auto p-4 flex flex-col items-center justify-end text-xl bg-[#262624] shadow-2xl rounded-3xl">
+      <div className="h-4/5 sm:h-4/5 w-full overflow-y-auto p-4 flex flex-col items-center justify-end text-xl bg-[#262624] shadow-2xl rounded-3xl">
         <MessageProvider>
           <Head />
           <Chatbox />
