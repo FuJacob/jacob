a18f303ea0eb22b669e138a17f826ab466c3bdfe
Jacob Fu <jjacobfu@gmail.com>
2025-04-22 12:34:29 -0400

    lets test this for mobile

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${noto_sans.variable} ${neuton.variable} antialiased max-w-screen-sm mx-auto`}
+        className={`${noto_sans.variable} ${neuton.variable} antialiased max-w-screen-sm min-h-screen overflow-y-auto mx-auto`}
       >
         {children}
       </body>
